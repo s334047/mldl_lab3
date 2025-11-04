6eb403ee8c7667ffd862e9ac68ae6a49b9fe3a6f
s334047 <s334047@studenti.polito.it>
2025-11-04 16:49:20 +0100

    prova

---
+++ models/customnet.py
@@ -0,0 +1,28 @@
+from torch import nn
+
+# Define the custom neural network
+class CustomNet(nn.Module):
+    def __init__(self):
+        super(CustomNet, self).__init__()
+        # Define layers of the neural network
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1, stride=2)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1, stride=2)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1, stride=2)
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=3, padding=1, stride=2)
+        self.conv5 = nn.Conv2d(512, 1024, kernel_size=3, padding=1, stride=2)
+
+        self.flatten = nn.Flatten(2)
+
+        # Add more layers...
+        self.fc1 = nn.Linear(1024, 200)  # 200 is the number of classes in TinyImageNet
+
+    def forward(self, x):
+        x = self.conv1(x).relu()
+        x = self.conv2(x).relu()
+        x = self.conv3(x).relu()
+        x = self.conv4(x).relu()
+        x = self.conv5(x).relu()
+
+        x = self.flatten(x).mean(-1)
+
+        return self.fc1(x)
